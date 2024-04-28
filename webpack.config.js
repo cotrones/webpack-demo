4bdc81a06a8c14610e839bea2318a254bdffb119
cotrones <tomcotroneo@gmail.com>
2024-04-28 02:48:55 -0400

    output filenames

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
-  },
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Caching",
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
