e84420b20f1e6d4ae63ff248d24a5e75ee14a538
cotrones <tomcotroneo@gmail.com>
2024-04-28 02:47:55 -0400

    caching preparation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-async function getComponent() {
+import _ from "lodash";
+import printMe from "./print.js";
+
+function component() {
   const element = document.createElement("div");
-  const { default: _ } = await import("lodash");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
