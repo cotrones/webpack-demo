fd5fdba3ed819002ec17d6da52fe7e68e192d8da
cotrones <tomcotroneo@gmail.com>
2024-04-28 02:14:28 -0400

    code splitting entry points

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,10 @@ module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
+    another: "./src/another-module.js",
   },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-    }),
-  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
   },
 };
